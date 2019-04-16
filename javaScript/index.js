297c39dbf66845461300270a12cbceb71796b6b9
ZacharyRay <47400499+ZacharyRay@users.noreply.github.com>
2019-04-16 14:07:54 +0200

    spørgeskema + index javascript
    Spørgeskemaet er blevet stylet og på index er der tilføjet javascript til glemt brugernavn og kode.
    
    Co-Authored-By: emilstorta <emilstorta@users.noreply.github.com>

---
+++ javaScript/index.js
@@ -0,0 +1,7 @@
+function glemtbruger() {
+    let bruger = prompt("Hvis du har glemt dit brugernavn, indtast din email her:");
+}
+
+function glemtkode() {
+    let bruger = prompt("Hvis du har glemt din kode, indtast din email her:");
+}
