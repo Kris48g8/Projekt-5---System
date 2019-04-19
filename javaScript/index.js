8ddf05b823bfeffed0c4ee541331c8d1f5704935
ZacharyRay <47400499+ZacharyRay@users.noreply.github.com>
2019-04-19 11:59:15 +0200

    Medarbejder siden review
    medarbejder siderne er næsten færdig, du kan nu vælge en kollega via dropdown menu

--- javaScript/index.js
+++
@@ -1,7 +0,0 @@
-function glemtbruger() {
-    let bruger = prompt("Hvis du har glemt dit brugernavn, indtast din email her:");
-}
-
-function glemtkode() {
-    let bruger = prompt("Hvis du har glemt din kode, indtast din email her:");
-}
--- javaScript/javaScript.js
+++ javaScript/javaScript.js
@@ -21,3 +21,48 @@ function myFunctionTwo() {
     x.style.display = "block";
   }
 }
+
+
+/* vælg kollegaer knap */
+
+/* show and hide funtion */
+
+
+
+
+function dropdown() {
+ document.getElementById("dropdownjavascript").classList.toggle("vis");
+}
+
+/* luk dropdown når man klikker udenfor boksen */
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdownbutton')) {
+    var dropdowns = document.getElementsByClassName("dropdownindhold");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('vis')) {
+        openDropdown.classList.remove('vis');
+      }
+    }
+  }
+}
+
+
+
+
+/* index alert/prompt */
+
+function glemtbruger() {
+    let bruger = prompt("Hvis du har glemt dit brugernavn, indtast din email her:");
+}
+
+function glemtkode() {
+    let bruger = prompt("Hvis du har glemt din kode, indtast din email her:");
+}
+
+
+
+
+
