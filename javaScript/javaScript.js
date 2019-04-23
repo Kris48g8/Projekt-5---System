cc88b2d9b75d73bdf35b6a452fc39241609d5ccc
Kristoffer <Kristoffer.sg123@gmail.com>
2019-04-23 12:49:33 +0200

    Validator rettelser, Js fix - manglende tags

--- javaScript/javaScript.js
+++ javaScript/javaScript.js
@@ -65,22 +65,3 @@ function glemtkode() {
 
 //Autosave funktion
 
-
-document.addEventListener("DOMContentLoaded", function() {
-    manuelAutosaver();
-});
-var active = false;
-function manuelAutosaver() {
-    window.setInterval(function() {
-        if (active === false) {
-            active = true;
-            /* Javascript der kalder hvordan et css skal display i dette tilfælde "block"(visible) */
-            document.getElementById('notifier').style.display = "block";
-        } else {
-            active = false;
-            /* Javascript der kalder hvordan et css skal display i dette tilfælde "none"(invisible) */
-            document.getElementById('notifier').style.display = "none";
-
-        }
-        }, 2500);
-}
